e9d1dee4b4e1fcf5ef10c4c4b408a4c985bafc77
ian-thomas-41 <60229585+ian-thomas-41@users.noreply.github.com>
2020-03-20 11:36:23 -0600

    1st remote lesson
    adding css and finishing detailview testing. plus, background image of the legendary nathaniel dwight lambert

--- polls/views.py
+++ polls/views.py
@@ -16,7 +16,7 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+    question = get_object_or_404(Question, pk=question_id, pub_date__lte=timezone.now())
     return render(request, 'polls/detail.html', {'question' : question})
 
 def results(request, question_id):
@@ -37,3 +37,4 @@ def vote(request, question_id):
         selected_choice.votes +=1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
